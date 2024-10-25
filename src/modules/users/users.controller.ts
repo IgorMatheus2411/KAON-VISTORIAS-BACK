8d504bc4978a8e2a089c06e0b71da85ee45e235b
Igor Occon <occon24@gmail.com>
2024-10-25 12:25:43 -0300

    LOGIN, LOGOUT 100%

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,11 +1,14 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   Param,
   Post,
   Put,
+  HttpException,
+  HttpStatus,
+  Patch,
+  Request,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDTO } from './user.dto';
@@ -17,46 +20,104 @@ export class UsersController {
   @Post('login')
   async login(@Body() userDto: UserDTO) {
     try {
-      console.log('Login request:', userDto);
-      return await this.userService.loginUser(userDto.email, userDto.password);
+      const { email, password } = userDto;
+      return await this.userService.loginUser(email, password);
     } catch (error) {
       console.error('Error during login:', error);
+      throw new HttpException(
+        'Erro ao fazer login. Verifique suas credenciais.',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
   }
 
-  @Post('refresh-token')
-  async refreshAccessToken(@Body('refreshToken') refreshToken: string) {
-    return this.userService.refreshAccessToken(refreshToken);
-  }
-
   @Post('logout')
-  async logout(@Body('userId') userId: string) {
-    return this.userService.logout(userId);
+  async logout(@Body('id') id: string) {
+    try {
+      await this.userService.logout(id);
+      return { message: 'Logout realizado com sucesso.' }; // Adicione esta linha
+    } catch (error) {
+      console.error('Error during logout:', error);
+      throw new HttpException(
+        'Erro ao fazer logout.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Post()
   async create(@Body() data: UserDTO) {
-    return this.userService.createUser(data);
+    try {
+      return await this.userService.createUser(data);
+    } catch (error) {
+      console.error('Error during user creation:', error);
+      throw new HttpException('Erro ao criar usuário.', HttpStatus.BAD_REQUEST);
+    }
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
+    try {
+      return await this.userService.findOne(id);
+    } catch (error) {
+      console.error('Error during user retrieval:', error);
+      throw new HttpException('Usuário não encontrado.', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @Get('status')
+  async getUserStatus(@Request() req): Promise<{ isActive: boolean }> {
+    const userId = req.user.id; // Assumindo que o ID do usuário está em req.user
+    const { isActive } = await this.userService.getUserStatus(userId);
+
+    if (!isActive) {
+      // Se o usuário não estiver ativo, faça o logout
+      await this.userService.logout(userId); // Chame o método de logout aqui
+      throw new HttpException(
+        'Usuário desativado. Logout realizado.',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    return { isActive };
   }
 
   @Get()
   async findAll() {
-    return this.userService.findAll();
+    try {
+      return await this.userService.findAll();
+    } catch (error) {
+      console.error('Error during users retrieval:', error);
+      throw new HttpException(
+        'Erro ao buscar usuários.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  // http://localhost:3000/123
   @Put(':id')
   async update(@Param('id') id: string, @Body() data: UserDTO) {
-    return this.userService.updateUser(id, data);
+    try {
+      return await this.userService.updateUser(id, data);
+    } catch (error) {
+      console.error('Error during user update:', error);
+      throw new HttpException(
+        'Erro ao atualizar usuário.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
-    return this.userService.deleteUser(id);
+  @Patch(':id')
+  async desactiveUserById(@Param('id') id: string) {
+    try {
+      return await this.userService.desactiveUser(id);
+    } catch (error) {
+      console.error('Error during user deletion:', error);
+      throw new HttpException(
+        'Erro ao deletar usuário.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,34 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { UserDTO } from './user.dto';
-import { UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
-
 import { createClient } from '@supabase/supabase-js';
+import { User } from '@prisma/client';
 
+// Configurações do Supabase
 const supabaseUrl = 'https://zvcycdfkcmmakmhfikhe.supabase.co';
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+// Configurações dos segredos JWT
+const jwtSecret = process.env.JWT_SECRET || 'seu-segredo-de-jwt';
+const jwtRefreshSecret =
+  process.env.JWT_REFRESH_SECRET || 'seu-segredo-refresh';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async loginUser(email: string, password: string) {
+  async loginUser(
+    email: string,
+    password: string,
+  ): Promise<{ accessToken: string; refreshToken: string; user: User }> {
     if (!email || !password) {
       throw new Error('Email e senha são obrigatórios.');
     }
 
+    // Tentar autenticar o usuário no Supabase
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
 
-    if (error) {
-      throw new Error(`Erro ao fazer login: ${error.message}`);
+    if (error || !data.user) {
+      throw new UnauthorizedException(
+        'Erro ao fazer login: ' + (error?.message || 'Usuário não encontrado.'),
+      );
     }
-    return data.user;
+
+    // Verificar se o usuário está ativo no banco de dados
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new UnauthorizedException(
+        'Usuário não encontrado no banco de dados.',
+      );
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('Usuário está inativo.');
+    }
+
+    // Gerar o accessToken e o refreshToken
+    const accessToken = this.generateAccessToken(user);
+    const refreshToken = this.generateRefreshToken(user);
+
+    // Atualizar o refreshToken no banco de dados
+    await this.prisma.user.update({
+      where: { email },
+      data: { refreshToken },
+    });
+
+    return { accessToken, refreshToken, user };
   }
 
   async createUser(data: UserDTO) {
@@ -41,94 +75,77 @@ export class UsersService {
         password: hashedPassword,
       },
     });
-    return user;
-  }
 
-  async validatePassword(
-    storedPassword: string,
-    inputPassword: string,
-  ): Promise<boolean> {
-    return bcrypt.compare(inputPassword, storedPassword); // bcrypt já faz a comparação diretamente
-  }
-
-  async login(
-    email: string,
-    password: string,
-  ): Promise<{ accessToken: string; refreshToken: string }> {
-    const user = await this.prisma.user.findUnique({ where: { email } });
-    if (!user) {
-      throw new UnauthorizedException('Usuário não encontrado');
-    }
-
-    const isValidPassword = await this.validatePassword(
-      user.password,
-      password,
-    );
-    if (!isValidPassword) {
-      throw new UnauthorizedException('Senha inválida');
-    }
-
-    const accessToken = this.generateAccessToken(user);
+    // Geração de refreshToken ao criar usuário
     const refreshToken = this.generateRefreshToken(user);
-
     await this.prisma.user.update({
       where: { id: user.id },
       data: { refreshToken },
     });
 
-    return { accessToken, refreshToken };
+    return user;
   }
 
-  generateAccessToken(user: any): string {
-    return jwt.sign({ id: user.id, email: user.email }, 'seu-segredo-de-jwt', {
+  async getUserByRefreshToken(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email }, // Busca diretamente pelo refreshToken
+    });
+    return user;
+  }
+
+  generateAccessToken(user: User): string {
+    return jwt.sign({ id: user.id, email: user.email }, jwtSecret, {
       expiresIn: '15m',
     });
   }
 
-  generateRefreshToken(user: any): string {
-    return jwt.sign({ id: user.id, email: user.email }, 'seu-segredo-refresh', {
+  generateRefreshToken(user: User): string {
+    return jwt.sign({ id: user.id, email: user.email }, jwtRefreshSecret, {
       expiresIn: '7d',
     });
   }
 
   async refreshAccessToken(
     refreshToken: string,
-  ): Promise<{ accessToken: string; newRefreshToken: string }> {
+  ): Promise<{ user: User; accessToken: string; newRefreshToken: string }> {
+    // Verifique o refreshToken e valide-o usando jsonwebtoken
+    let decoded: any;
     try {
-      const payload = jwt.verify(refreshToken, 'seu-segredo-refresh') as any;
-      const user = await this.prisma.user.findUnique({
-        where: { id: payload.id },
-      });
-
-      if (!user || user.refreshToken !== refreshToken) {
-        throw new Error('Refresh token inválido');
-      }
-
-      // Gerar novo refresh token e access token
-      const newAccessToken = this.generateAccessToken(user);
-      const newRefreshToken = this.generateRefreshToken(user);
-
-      // Atualiza o refresh token no banco
-      await this.prisma.user.update({
-        where: { id: user.id },
-        data: { refreshToken: newRefreshToken },
-      });
-
-      return { accessToken: newAccessToken, newRefreshToken };
+      decoded = jwt.verify(refreshToken, jwtRefreshSecret); // Decodifica e verifica o token
     } catch (error) {
-      // Primeiro logue o erro no console
-      console.log('Erro ao renovar token:', error);
-
-      // Depois lance um novo erro
-      throw new Error('Erro ao renovar token');
+      console.log(error);
+      throw new UnauthorizedException('Refresh token inválido.');
     }
+
+    const user = await this.prisma.user.findUnique({
+      where: { id: decoded.id },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Usuário não encontrado.');
+    }
+
+    // Gerar novos tokens
+    const accessToken = this.generateAccessToken(user);
+    const newRefreshToken = this.generateRefreshToken(user);
+
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: { refreshToken: newRefreshToken },
+    });
+
+    // Retornar o usuário junto com os novos tokens
+    return { user, accessToken, newRefreshToken };
   }
 
-  async logout(userId: string) {
-    return this.prisma.user.update({
-      where: { id: userId },
+  async logout(id: string) {
+    await this.prisma.user.update({
+      where: { id },
       data: { refreshToken: null },
     });
+    await supabase.auth.signOut();
+
+    return { message: 'Logout realizado com sucesso.' };
   }
 
   async findOne(id: string) {
@@ -138,7 +155,6 @@ export class UsersService {
         id: true,
         name: true,
         email: true,
-        // Excluir password e refreshToken
         vistorias: {
           select: {
             cliente: true,
@@ -153,7 +169,7 @@ export class UsersService {
       include: {
         vistorias: {
           select: {
-            cliente: true, // Puxa apenas o campo 'cliente' da vistoria
+            cliente: true,
           },
         },
       },
@@ -161,39 +177,44 @@ export class UsersService {
   }
 
   async updateUser(id: string, data: UserDTO) {
-    const userExists = await this.prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
+    // Verifica se o usuário existe com base no ID
+    const userExists = await this.prisma.user.findUnique({ where: { id } });
 
     if (!userExists) {
-      throw new Error('Usuario não existe!');
+      throw new Error('Usuário não existe!');
     }
 
+    // Atualiza o usuário com os dados fornecidos
     return await this.prisma.user.update({
+      where: { id }, // Use o ID para atualizar o usuário
       data,
-      where: {
-        id,
-      },
     });
   }
 
-  async deleteUser(id: string) {
-    const userExists = await this.prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    if (!userExists) {
-      throw new Error('Usuario não existe!');
+  async desactiveUser(id: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      throw new Error('Usuário não existe!');
     }
 
-    return await this.prisma.user.delete({
-      where: {
-        id,
-      },
+    // Atualizar o campo isActive para false em vez de deletar o usuário
+    const isActive = !user.isActive; // Inverte o status
+    return await this.prisma.user.update({
+      where: { id },
+      data: { isActive, refreshToken: null },
     });
+  }
+
+  async getUserStatus(userId: string): Promise<{ isActive: boolean }> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: { isActive: true },
+    });
+
+    if (!user) {
+      throw new Error('Usuário não encontrado!');
+    }
+
+    return { isActive: user.isActive };
   }
 }
