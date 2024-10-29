9d48a729747d502c438da6f53c76929a112003b9
Igor <occon24@gmail.com>
2024-10-28 22:24:48 -0300

    vercel with code

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 3000; // Porta dinâmica
+  await app.listen(port);
   app.enableCors(); // Habilita CORS para todas as origens
-  await app.listen(3000);
 }
 
 bootstrap();
