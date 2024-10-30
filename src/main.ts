b350d9d9a1126130b79f9b25e6959417872d1851
Igor <occon24@gmail.com>
2024-10-29 22:40:05 -0300

    2.2 render

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 
+// Cria uma aplicação Express
 const expressApp = express();
 
+// Define uma rota padrão do Express
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!'); // Rota simples para verificar se o servidor está funcionando
+});
+
+// Função para inicializar a aplicação NestJS
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
@@ -27,3 +34,11 @@ export const handler = async (req, res) => {
   const app = await bootstrap(); // Inicializa o Nest
   return app(req, res); // Passa as requisições e respostas para o express
 };
+
+// Inicia o servidor localmente se não estiver no ambiente Vercel
+const port = process.env.PORT || 3000;
+if (process.env.VERCEL_ENV !== 'production') {
+  expressApp.listen(port, () => {
+    console.log(`Application is running on: http://localhost:${port}`);
+  });
+}
