a39617a278e1e317d60842e3f578a9174f701535
Igor <occon24@gmail.com>
2024-10-29 21:49:37 -0300

    1.6 vercel

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express'; // Importando express corretamente
+import express from 'express';
 
 const expressApp = express();
 
 async function bootstrap() {
-  // Crie o aplicativo NestJS usando o adaptador do Express
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressApp),
   );
-
-  // Inicialize o aplicativo
   await app.init();
 }
 
-// Inicializa o aplicativo
 bootstrap();
 
-export const handler = expressApp; // Exportando a instância express
+export const handler = expressApp; // Exporta o adaptador do Express
