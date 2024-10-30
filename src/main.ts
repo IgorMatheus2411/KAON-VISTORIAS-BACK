058719e1dcfa0c6f83b1ec460cd11aef40c8ef56
Igor Occon <occon24@gmail.com>
2024-10-30 06:45:31 -0300

    2.4 render

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
-// Cria uma aplicação Express
-const expressApp = express();
-
-// Define uma rota padrão do Express
-expressApp.get('/', (req, res) => {
-  res.send('Hello World!'); // Rota simples para verificar se o servidor está funcionando
-});
-
-// Função para inicializar a aplicação NestJS
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
+  const app = await NestFactory.create(AppModule);
 
-  // Inicializa a aplicação Nest
-  await app.init();
+  // Ativa CORS se for necessário (para chamadas de outras origens)
+  app.enableCors();
 
-  // Define a rota padrão para o Express
-  expressApp.get('/(.*)', (req, res) =>
-    app.getHttpAdapter().getInstance().handle(req, res),
-  );
+  // Define a porta para o Render
+  const port = process.env.PORT || 3000;
 
-  return expressApp; // Retorna o manipulador express
-}
-
-// Exporte o manipulador para o Vercel
-export const handler = async (req, res) => {
-  const app = await bootstrap(); // Inicializa o Nest
-  return app(req, res); // Passa as requisições e respostas para o express
-};
-
-// Inicia o servidor localmente se não estiver no ambiente Vercel
-const port = process.env.PORT || 3000;
-if (process.env.VERCEL_ENV !== 'production') {
-  expressApp.listen(port, () => {
+  await app.listen(port, () => {
     console.log(`Application is running on: http://localhost:${port}`);
   });
 }
+
+bootstrap();
