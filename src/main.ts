0ba97a8b0e9d4382ae33c071eac4eb22509cafb3
Igor <occon24@gmail.com>
2024-10-29 22:10:22 -0300

    1.9 vercel

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,28 @@ async function bootstrap() {
     AppModule,
     new ExpressAdapter(expressApp),
   );
+
+  // Inicializa a aplicação Nest
   await app.init();
+
+  // Define a rota padrão para o Express
+  expressApp.get('/(.*)', (req, res) =>
+    app.getHttpAdapter().getInstance().handle(req, res),
+  );
+
+  // Inicia o servidor na porta 3000 apenas em ambiente local
+  const port = process.env.PORT || 3000; // Use a porta do ambiente ou 3000
+  if (process.env.VERCEL_ENV !== 'production') {
+    expressApp.listen(port, () => {
+      console.log(`Application is running on: http://localhost:${port}`);
+    });
+  }
 }
 
-bootstrap();
-
+// Exporte o manipulador para o Vercel
 export const handler = expressApp;
+
+// Chame a função bootstrap
+bootstrap().catch((error) => {
+  console.error('Error starting the application:', error);
+});
